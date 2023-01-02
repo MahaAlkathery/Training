2310de25abcf00d640ee44da2fc1b98fdc29229b
MahaAlkathery <120279449+MahaAlkathery@users.noreply.github.com>
2023-01-02 10:30:04 +0300

    New orgnization

--- Access a file/access.py
+++
@@ -1,9 +0,0 @@
-f = open("text.txt",'r')
-contents =f.read()
-print(contents)
-f.close()
-
-try:
-    f = open("DoesNotExist.file",'r')
-except FileNotFoundError as error:
-    print("you should check that \'DoesNotExist.file\'")
